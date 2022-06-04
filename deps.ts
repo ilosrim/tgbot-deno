115759d57c30d1c22b7fe0cbe556da0d57b821c9
ilosrim <ilosrim@yahoo.com>
2022-06-04 08:28:20 -0700

    edit mod & deps file

--- deps.ts
+++ deps.ts
@@ -1,3 +1,5 @@
-export { Bot } from "https://deno.land/x/telegram@v0.1.1/mod.ts";
+// export { Bot } from "https://deno.land/x/telegram@v0.1.1/mod.ts";
+
+export { Bot } from "https://deno.land/x/grammy/mod.ts";
 
 import "https://deno.land/x/dotenv/load.ts";
--- mod.ts
+++ mod.ts
@@ -5,15 +5,30 @@ const token = Deno.env.get('BOT_TOKEN') as string;
 
 console.log(token);
 
-
 const bot = new Bot(token);
 
-bot.on("text", async (ctx) => {
-  const text = ctx.message?.text;
+// deno telegram 
 
-  if (text === '/shibe'){
-    await ctx.reply(await getShibes());
-  }
-})
+// bot.on("text", async (ctx) => {
+//   const text = ctx.message?.text;
 
-bot.launch();
+//   if (text === '/shibe'){
+//     await ctx.reply(await getShibes());
+//   }
+// })
+
+// bot.launch();
+
+// deno grammy
+bot.command("start", (ctx) => ctx.reply("Welcome, send me a photo!"));
+bot.on("message:text", (ctx) => ctx.reply("That is text not a photo!"));
+bot.on("message:photo", (ctx) => ctx.reply("Nice photo, is that you?"));
+bot.on(
+  "edited_message",
+  (ctx) => 
+    ctx.reply("Ha! Gotcha! You just edited this!", {
+      reply_to_message_id: ctx.editedMessage.message_id,
+    }),
+);
+
+bot.start();
